a67d47b21e2d3ef707ce85300bc8aecdf1f92df4
immaculate <nayigaimmaculate098@gmail.com>
2024-07-09 13:06:38 +0300

    factor2

--- factorial.test.js
+++ factorial.test.js
@@ -6,9 +6,9 @@ describe('factorial', () => {
     assert.equal(factorial(0), 1);
   });
 
-  // it('should return 1 for factorial of 1', () => {
-  //   assert.equal(factorial(1), 1);
-  // });
+  it('should return 1 for factorial of 1', () => {
+    assert.equal(factorial(1), 1);
+  });
 
   // it('should return 2 for factorial of 2', () => {
   //   assert.equal(factorial(2), 2);
